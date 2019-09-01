8842301f55fb83206d1f15537c2020893603ef5a
eeyshen <david.enya.shen@gmail.com>
2019-09-01 20:45:39 +0800

    update

--- packages/react-vega-lite/src/VegaLite.jsx
+++ packages/react-vega-lite/src/VegaLite.jsx
@@ -7,6 +7,7 @@ const VegaLite = props => {
   const parsedProps = { ...props };
   const { type, spec, data } = props;
   if (type === "vega-lite") {
+    console.log("test");
     const combinedSpec = { ...spec };
     if (data) {
       combinedSpec.data = data;
